775078633949faecd978fdb21804a28019ca13e7
Max Reinmueller <maxreinmueller@gmail.com>
2019-10-06 09:53:31 -0700

    switching over portfolio

---
+++ models/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS flashee;
+CREATE DATABASE flashee;
+
+USE flashee;
+
+CREATE TABLE cards (
+cardTitle VARCHAR(100),
+term VARCHAR (100),
+definition VARCHAR (200),
+user VARCHAR (100)
+);
+
