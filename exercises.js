702dddce2ef8c2c80b9818dbf5ec455ccc17b373
Max Reinmueller <maxreinmueller@gmail.com>
2020-04-28 15:20:28 -0700

    added js

--- exercises.js
+++ exercises.js
@@ -19,3 +19,22 @@ var countZeros = (newNumber) => {
     }
     console.log('count ' + count)
 }
+
+$("#formId").submit(function() {
+    var initalInput = $("#theValue").val() 
+    $("#theValue").val("")
+    event.preventDefault();
+    splitReverse(initalInput);
+});
+
+const splitReverse = (initalInput) => {
+    var reverseInput = initalInput.split("").reverse().join("");
+    display(reverseInput)
+    event.preventDefault();
+}
+
+const display = (reverseInput) => {
+    var newDiv = $("<h4 class='card-text'>")
+    newDiv.text(reverseInput)
+    $("#answer").append(newDiv);
+}
