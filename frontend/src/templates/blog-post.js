3fca552f9e834a23ecbf34e414dc047f5b8f3b87
Max Reinmueller <maxreinmueller@gmail.com>
2020-07-02 13:00:12 -0700

    added bluma

--- frontend/src/templates/blog-post.js
+++ frontend/src/templates/blog-post.js
@@ -79,23 +79,3 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
 }
 
 export default BlogPostTemplate
-
-export const pageQuery = graphql`
-  query BlogPostBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-  }
-`
