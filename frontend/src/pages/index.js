d603fa37e681fe30f29f0dfb79b36368b1c0ffcb
Max Reinmueller <maxreinmueller@gmail.com>
2020-07-09 20:36:15 -0700

    added navbar component

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,20 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import NavBar from "../components/NavBar"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
+    <NavBar />
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <button className="py-4 px-4 bg-blue-600 hover:bg-blue-900">Ba Boom!</button>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+
+  </div>
 )
 
 export default IndexPage
