61409ecce39a68709e6e40294153f8f17653d9f9
Max Reinmueller <maxreinmueller@gmail.com>
2020-07-15 18:29:21 -0700

    added recent work section and css

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,6 +5,9 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import NavBar from "../components/NavBar"
+import WorkCard from "../components/workCard"
+
+
 
 const IndexPage = () => (
   <div>
@@ -17,13 +20,20 @@ const IndexPage = () => (
         <p class="text-gray-700 text-base text-center">I design and develop delightful experiences</p>
       </div>
     </div>
+    {/* introduction and background */}
     <div class=" justify-center center-content flex-wrap bg-blue-400">
       <div class="px-6 py-12 mx-6 content-center">
-        <div class="text-xl font bold mb-2 mt-4 text-center">Hello world, My name is Max</div>
+        <div class="text-xl font-bold mb-2 mt-4 text-center">Hello world, My name is Max</div>
         <p class="text-base text-center">I am a UX designer and web developer passionate about creating delightfully intuitive experiences. My background is in assisting people and brands with connecting to their audience and bringing their ideas to life.</p>
       </div>
     </div>
-  
+  {/* recent work */}
+  <div class=" justify-center center-content flex-wrap bg-white">
+      <div class="px-6 py-12 mx-6 content-center">
+        <div class="text-xl font-bold mb-2 mt-4 text-center">My Recent Work</div>
+        <WorkCard class=""/>
+      </div>
+    </div>
   </div>
 )
 
