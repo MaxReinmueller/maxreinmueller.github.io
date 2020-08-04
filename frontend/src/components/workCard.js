2344453b18d007c47cf01491c3904bce3bdc23b6
Max Reinmueller <maxreinmueller@gmail.com>
2020-08-04 12:17:45 -0700

    published site

--- frontend/src/components/workCard.js
+++ frontend/src/components/workCard.js
@@ -23,7 +23,7 @@ const WorkCard = () => {
       <ManoothLogo class="w-full" />
         <div class="px-6 py-4">
           <div class="font-sans font-family:Helvetica-Neue font-medium text-xl mb-2">Maynooth Furniture</div>
-          <p class="text-gray-700 text-base">UX project for a fictitious furniture store designed with adobe XD and developed with Gatsby and Tailwind CSS </p>
+          <p class="text-gray-700 text-base">UX project for a fictitious furniture store designed with adobe XD. </p>
          <a href="https://xd.adobe.com/view/4a2a2dbd-a165-48c0-8284-4b66cd22e137-1f79/?fullscreen" target="https://xd.adobe.com/view/4a2a2dbd-a165-48c0-8284-4b66cd22e137-1f79/?fullscreen"> <button class="absolute bottom-0 mb-4 bg-white hover:bg-gray-100 hover:shadow-inner text-gray-800 font-semibold py-2 px-4 rounded shadow-lg">
             Go
           </button> </a>
